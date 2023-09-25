ee897d471aa7c69bd15348f23266d3526a9394ac
prarambhak <kafleprarambha@gmail.com>
2023-09-25 18:21:44 +0545

    added every other file

---
+++ signup.js
@@ -0,0 +1,17 @@
+function myFunction() {
+    var x = document.getElementById("myInput");
+    if (x.type === "password") {
+      x.type = "text";
+    } else {
+      x.type = "password";
+    }
+  }
+  function handleCheckboxClick(checkbox) {
+    var checkboxes = document.getElementsByName('myCheckbox');
+
+    for (var i = 0; i < checkboxes.length; i++) {
+        if (checkboxes[i] !== checkbox) {
+            checkboxes[i].checked = false;
+        }
+    }
+}
