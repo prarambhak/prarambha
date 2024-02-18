b962172bc4b64bb2a2ac49a9534208bdd05f24d3
prarambhak <kafleprarambha@gmail.com>
2024-02-18 21:10:49 +0545

    Changing signup form

--- signup.js
+++ signup.js
@@ -1,18 +1,11 @@
-function myFunction() {
-    var x = document.getElementById("myInput");
-    if (x.type === "password") {
-      x.type = "text";
-    }
-    else {
-      x.type = "password";
-    }
+function togglePasswordVisibility(inputId) {
+  const input = document.getElementById(inputId);
+  const toggleBtn = document.querySelector(`#${inputId} + .toggle-password`);
+  if (input.type === "password") {
+      input.type = "text";
+      toggleBtn.textContent = "Hide";
+  } else {
+      input.type = "password";
+      toggleBtn.textContent = "Show";
   }
-  function handleCheckboxClick(checkbox) {
-    var checkboxes = document.getElementsByName('myCheckbox');
-
-    for (var i = 0; i < checkboxes.length; i++) {
-        if (checkboxes[i] !== checkbox) {
-            checkboxes[i].checked = false;
-        }
-    }
 }
